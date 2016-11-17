8e39b1eb02376f659e7846d39ecacf022e6cacd7
KBoreo <kboreo@ltu.edu>
2016-11-17 07:21:31 -0500

    Added header and standard namespace

--- SD4/SD4/SD4.cpp
+++ SD4/SD4/SD4.cpp
@@ -1,12 +1,27 @@
-#include <iostream>
-using namespace std;
+/*********************************************************************************************************************************
+**
+** Module Name:  SD$
+**
+** Module Description:
+**  This is the main module.
+**
+**
+**********************************************************************************************************************************
+**
+** Author:			Kodey Boreo, Ivoire Morrell, Dake Shi, Ali Alfaryhin, Nicholas Hydorn
+** Revision:        1.0
+** Creation Date:   16-Nov-2016
+**
+**********************************************************************************************************************************/
+
 
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
 
 void main()
 {
 	cout << "Hello World!" << endl;
 	system("pause");
-}//test1
-//1111
+}
